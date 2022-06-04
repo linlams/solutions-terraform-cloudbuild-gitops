a1403d71ddd2df9bdaa21e7649970a621965f75f
linlams <linlams@gmail.com>
2022-06-04 19:48:27 +0800

    Update outputs.tf

--- modules/db_server/outputs.tf
+++ modules/db_server/outputs.tf
@@ -1,11 +1,11 @@
-/*
- * Copyright 2017 Google Inc.
+/**
+ * Copyright 2019 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,30 +14,120 @@
  * limitations under the License.
  */
 
+// Master
 output "instance_name" {
-  description = "The name of the database instance"
-  value       = google_sql_database_instance.master.name
+  value       = google_sql_database_instance.default.name
+  description = "The instance name for the master instance"
 }
 
-output "instance_address" {
-  description = "The IPv4 address of the master database instnace"
-  value       = google_sql_database_instance.master.ip_address.0.ip_address
+output "instance_ip_address" {
+  value       = google_sql_database_instance.default.ip_address
+  description = "The IPv4 address assigned for the master instance"
 }
 
-output "instance_address_time_to_retire" {
-  description = "The time the master instance IP address will be retired. RFC 3339 format."
-  value       = google_sql_database_instance.master.ip_address.0.time_to_retire
+output "private_address" {
+  value       = google_sql_database_instance.default.private_ip_address
+  description = "The private IP address assigned for the master instance"
 }
 
-output "self_link" {
-  description = "Self link to the master instance"
-  value       = google_sql_database_instance.master.self_link
+output "instance_first_ip_address" {
+  value       = google_sql_database_instance.default.first_ip_address
+  description = "The first IPv4 address of the addresses assigned for the master instance."
+}
+
+output "instance_connection_name" {
+  value       = google_sql_database_instance.default.connection_name
+  description = "The connection name of the master instance to be used in connection strings"
+}
+
+output "instance_self_link" {
+  value       = google_sql_database_instance.default.self_link
+  description = "The URI of the master instance"
+}
+
+output "instance_server_ca_cert" {
+  value       = google_sql_database_instance.default.server_ca_cert
+  description = "The CA certificate information used to connect to the SQL instance via SSL"
+}
+
+output "instance_service_account_email_address" {
+  value       = google_sql_database_instance.default.service_account_email_address
+  description = "The service account email address assigned to the master instance"
+}
+
+// Replicas
+output "replicas_instance_first_ip_addresses" {
+  value       = [for r in google_sql_database_instance.replicas : r.ip_address]
+  description = "The first IPv4 addresses of the addresses assigned for the replica instances"
+}
+
+output "replicas_instance_connection_names" {
+  value       = [for r in google_sql_database_instance.replicas : r.connection_name]
+  description = "The connection names of the replica instances to be used in connection strings"
+}
+
+output "replicas_instance_self_links" {
+  value       = [for r in google_sql_database_instance.replicas : r.self_link]
+  description = "The URIs of the replica instances"
+}
+
+output "replicas_instance_server_ca_certs" {
+  value       = [for r in google_sql_database_instance.replicas : r.server_ca_cert]
+  description = "The CA certificates information used to connect to the replica instances via SSL"
+}
+
+output "replicas_instance_service_account_email_addresses" {
+  value       = [for r in google_sql_database_instance.replicas : r.service_account_email_address]
+  description = "The service account email addresses assigned to the replica instances"
+}
+
+output "read_replica_instance_names" {
+  value       = [for r in google_sql_database_instance.replicas : r.name]
+  description = "The instance names for the read replica instances"
 }
 
 output "generated_user_password" {
-  description = "The auto generated default user password if no input password was provided"
-  value       = random_id.user-password.hex
+  description = "The auto generated default user password if not input password was provided"
+  value       = random_password.user-password.result
   sensitive   = true
 }
 
+output "additional_users" {
+  description = "List of maps of additional users and passwords"
+  value = [for r in google_sql_user.additional_users :
+    {
+      name     = r.name
+      password = r.password
+    }
+  ]
+  sensitive = true
+}
 
+output "public_ip_address" {
+  description = "The first public (PRIMARY) IPv4 address assigned for the master instance"
+  value       = google_sql_database_instance.default.public_ip_address
+}
+
+output "private_ip_address" {
+  description = "The first private (PRIVATE) IPv4 address assigned for the master instance"
+  value       = google_sql_database_instance.default.private_ip_address
+}
+
+// Resources
+output "primary" {
+  value       = google_sql_database_instance.default
+  description = "The `google_sql_database_instance` resource representing the primary instance"
+  sensitive   = true
+}
+
+output "replicas" {
+  value       = values(google_sql_database_instance.replicas)
+  description = "A list of `google_sql_database_instance` resources representing the replicas"
+  sensitive   = true
+}
+
+output "instances" {
+  value       = concat([google_sql_database_instance.default], values(google_sql_database_instance.replicas))
+  description = "A list of all `google_sql_database_instance` resources we've created"
+  sensitive   = true
+}
