579997b9f8fbc6597b73613e058c5310cd54b10b
linlams <linlams@gmail.com>
2022-06-04 16:43:50 +0800

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -38,3 +38,9 @@ module "firewall" {
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
 }
+
+module "db_server" {
+  source  = "../../modules/db_server"
+  project = "${var.project}"
+  subnet  = "${module.vpc.subnet}"
+}
