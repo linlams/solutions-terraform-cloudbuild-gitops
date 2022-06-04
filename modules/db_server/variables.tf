eeba85199da7d477ae82ea4706c2d61cc9e6f1b6
linlams <linlams@gmail.com>
2022-06-04 19:10:02 +0800

    Update variables.tf

--- modules/db_server/variables.tf
+++ modules/db_server/variables.tf
@@ -13,11 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-variable "master" {
-  type        = "map"
-  description = "Master configuration"
-}
    
 variable "project" {}
 variable "subnet" {}
