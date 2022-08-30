1f58961b43b3728cb58361bfd5e6e1a1e9c2fc9e
linlams <linlams@gmail.com>
2022-08-30 11:36:57 +0000

    update server type

--- modules/db_server/main.tf
+++ modules/db_server/main.tf
@@ -1,5 +1,5 @@
 resource "google_redis_instance" "my_memorystore_redis_instance" {
-  name           = "myinstance"
+  name           = "myinstance-1"
   tier           = "BASIC"
   memory_size_gb = 2
   region         = "us-central1"
